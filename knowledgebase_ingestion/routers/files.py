b73db8feaf3770f903c0c88aaf2e96665d8ab9e5
ecommbalaji <globistaan@gmail.com>
2026-01-28 20:06:16 +0100

    Fix knowledgebase_ingestion service imports
    - Fix import from ..service.ingestion to ..service.ingestion_service
    - Remove unused ServiceFactory import
    - Fixes ModuleNotFoundError: No module named 'knowledgebase_ingestion.service.ingestion'

--- knowledgebase_ingestion/routers/files.py
+++ knowledgebase_ingestion/routers/files.py
@@ -8,10 +8,9 @@ from fastapi import APIRouter, UploadFile, File, Form, Header, HTTPException, Qu
 from ..schemas.models import (
     UploadResponse, FileInfo, BatchUploadResponse, BatchDeleteResponse, BatchUploadItem, BatchDeleteItem
 )
-from ..service.ingestion import (
+from ..service.ingestion_service import (
     process_single_file_upload, delete_file_logic, process_single_file_delete
 )
-from ..service.service_factory import ServiceFactory
 from ..utils.files import stream_to_temp_file, calculate_sha256
 from ..utils.validation import (
     sanitize_filename, validate_file_extension, validate_mime_type, validate_file_size, detect_mime_type_from_extension
